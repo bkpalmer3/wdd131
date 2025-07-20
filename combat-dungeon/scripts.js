3d26e89712f2f066547a45e900adc3dcb29ed543
Benjamin Palmer <144744973+bkpalmer3@users.noreply.github.com>
2025-07-19 22:57:33 -0600

    more work on project

--- combat-dungeon/scripts.js
+++ combat-dungeon/scripts.js
@@ -9,13 +9,19 @@ import { monsters } from './all-monsters.js';
           <p><strong>HP:</strong> <span id="health">${monster.hit_points}</span></p>
           <p><strong>AC:</strong> <span id="ac">${monster.armor_class}</span></p>
           <div class="ability-scores">
-              <p><strong>STR:</strong> ${monster.str} ${monster.str_mod} <strong>DEX:</strong> ${monster.dex} ${monster.dex_mod} <strong>CON:</strong> ${monster.con} ${monster.con_mod} <strong>INT:</strong> ${monster.int} ${monster.int_mod} <strong>WIS:</strong> ${monster.wis} ${monster.wis_mod} <strong>CHA:</strong> ${monster.cha} ${monster.cha_mod}</p>
-          </div>
-          <div class="traits hide">
-          <p><strong>Actions:</strong> <span id="actions">${monster.actions}</span>
-          <p><strong>Traits:</strong> <span id="traits">${monster.traits}</span>
-              <p><strong>Skills:</strong> <span id="skills">${monster.skills}</span>
-          </div>
+              <p><strong>STR:</strong> ${monster.str} ${monster.str_mod}</p> 
+              <p><strong>DEX:</strong> ${monster.dex} ${monster.dex_mod}</p>
+              <p><strong>CON:</strong> ${monster.con} ${monster.con_mod}</p>
+              <p><strong>INT:</strong> ${monster.int} ${monster.int_mod}</p>
+              <p><strong>WIS:</strong> ${monster.wis} ${monster.wis_mod}</p>
+              <p><strong>CHA:</strong> ${monster.cha} ${monster.cha_mod}</p>
+        </div>
+      </div>
+        <button class="bttn" id="bttn">Show More</button>
+      <div class="traits hide">
+        <p><strong>Actions:</strong> <span id="actions">${monster.actions}</span>
+        <p><strong>Traits:</strong> <span id="traits">${monster.traits}</span>
+        <p><strong>Skills:</strong> <span id="skills">${monster.skills}</span>
       </div>
 </div>`
   }
@@ -23,7 +29,7 @@ import { monsters } from './all-monsters.js';
 
   function renderCards(monsters){
     let container = document.querySelector('#card-container')
-    const card = createCardTemplate(monsters[0])
+    const card = createCardTemplate(monsters[203])
     container.innerHTML += card
 
   }
